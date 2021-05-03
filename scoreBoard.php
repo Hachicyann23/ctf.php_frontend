4852615e399cfd376c3aaff9bc82e97dfb7244e7
Hachicyann23 <oumatrofan23@gmail.com>
2021-05-04 06:26:17 +0800

    commit

---
+++ scoreBoard.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<script src="https://kit.fontawesome.com/b4f4eda484.js" crossorigin="anonymous"></script>
+	<title>Submenu1</title>
+</head>
+
+<body>
+<header>
+    <nav>
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn"><i class="fas fa-bars"></i></label>
+        <img class="logoImg" src="assets/img/ATL2021.png" alt="">
+        <label class="logo">ATL2021</label>
+        <ul>
+            <li><a class="" href="#">HOME</a></li>
+            <li><a class="" href="#">Category</a></li>
+            <li><a class="active" href="#">SCOREBOARD</a></li>
+        </ul>
+    </nav></header>
+    <section class="scoreContainer">
+        <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, quisquam. Quam, neque consequatur eum, consectetur iure veniam alias facilis ullam repellat voluptas natus, aliquid nihil aliquam exercitationem qui temporibus est corrupti provident itaque distinctio eligendi. Corrupti sint fuga quisquam eos soluta ut, doloribus molestias architecto laborum tempore repudiandae iusto adipisci harum odit, eligendi, quos accusantium non earum. Suscipit placeat, voluptates consequatur similique illum obcaecati numquam maxime saepe, officiis cupiditate, sint possimus quo accusantium. Aliquid, fugit deserunt. Odit reiciendis, tempore culpa facere neque itaque quas labore tempora, voluptates quaerat modi repellendus voluptate. Totam quod corrupti labore sint odit beatae consequuntur recusandae et deserunt aperiam possimus quis, deleniti adipisci praesentium autem tempore, dignissimos architecto vel eveniet consectetur cum. Repudiandae voluptates facere vel, asperiores, sed obcaecati quam ipsa deserunt nemo soluta fugit perspiciatis ex eum ut veritatis harum, recusandae hic porro. Aut accusamus earum deserunt quibusdam consequuntur, quam architecto! Iure, perferendis nobis. Blanditiis et ea fugiat unde iusto saepe expedita accusamus minus non, consequatur fugit deleniti veniam repellat rem eligendi natus assumenda in earum temporibus autem a praesentium. Dolorem, sunt? Officia recusandae iure fugiat, rem ducimus harum quis doloremque quam blanditiis porro non sequi. Magni ex exercitationem quasi eius totam quas asperiores nam, quam maiores, similique provident tempore aut nesciunt animi porro eos repudiandae? Quas ipsam officia vero quasi, a incidunt, pariatur molestiae quisquam delectus voluptates voluptas sed! Quas quo fugiat corrupti exercitationem porro ipsum quae est provident aperiam ipsa distinctio officia dolorum debitis quidem eius eaque, sunt ut eum quisquam. Ea, illo! Perferendis blanditiis veritatis quod. Totam quasi tenetur quis vel fugit? Officia dolorem dignissimos accusantium nobis eveniet ex minima mollitia a odit. Corrupti, laboriosam! Eaque fuga quasi debitis dolore mollitia quis veniam accusamus necessitatibus nam atque aliquid ratione quod repellendus blanditiis nesciunt temporibus dignissimos laborum ad nostrum fugit, sunt distinctio dicta? Dignissimos perferendis laboriosam quis nobis rerum assumenda necessitatibus repellendus sunt distinctio quaerat, est cumque pariatur, corrupti iusto accusantium culpa. Cum dolor repellendus itaque porro unde consectetur, alias voluptates? Expedita vel, neque, totam in aliquam, minima at molestiae quo minus saepe quas sunt. Non nobis debitis magnam, incidunt expedita blanditiis perspiciatis, corrupti excepturi, quos quam nam eius quia possimus doloribus ipsam dolor omnis hic delectus facilis nostrum dignissimos vitae alias. Cupiditate accusantium at saepe similique est. Natus totam praesentium cupiditate a officiis excepturi nemo voluptatibus assumenda optio ullam suscipit, earum ad non? Autem necessitatibus dolor doloremque neque quaerat in reprehenderit corporis, fuga amet, obcaecati tempora, labore dolorum commodi modi distinctio. Aperiam, quaerat? Minus nesciunt veritatis tempore illum nisi voluptates quasi unde dicta aliquid temporibus natus porro ea explicabo, nulla, quas facere sequi deleniti facilis commodi magnam, rem officiis quaerat. Corporis sequi sunt reiciendis consequatur et numquam exercitationem architecto maxime qui ad ex commodi eligendi, libero optio possimus ut nulla in aliquid deleniti, ullam suscipit, voluptate voluptatum? Maxime sapiente dicta excepturi deleniti minus nemo enim doloremque accusamus nam accusantium optio a quaerat, nihil repudiandae. Libero impedit soluta doloremque non aspernatur. Porro delectus soluta voluptas, repellendus sit facilis labore ea quis debitis temporibus?</h1>
+    </section>
+	<?php include "template/footer.temp.php"?>
+</body>
+
+</html>
